80cb45d42207fb15567c3096ebaac6b1f520ce9e
Max Morris <maximo1491@hotmail.com>
2022-12-01 10:55:29 +0000

    day1

--- day1.py
+++ day1.py
@@ -2,7 +2,29 @@ import helper
 
 def part1():
     input = helper.get_daily_input()
-    pass
+    current_count = 0
+    hightest_count = 0
+    for i in input:
+        if i == "\n":
+            if current_count > hightest_count:
+                hightest_count = current_count
+            current_count = 0
+            continue
+        current_count += i
+
+    return hightest_count
 
 def part2():
-    pass
+    input = helper.get_daily_input()
+    count = 0
+    values = []
+    for i in input:
+        if i == "\n":
+            values.insert(1, count)
+            count = 0
+            continue
+        count += i
+
+    values.sort(reverse=True)
+
+    return values[0] + values[1] + values[2]

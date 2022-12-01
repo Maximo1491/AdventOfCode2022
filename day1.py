75e51d18e6cbe5ae211bbbd2320b289bdcb1a527
Max Morris <maximo1491@hotmail.com>
2022-12-01 10:38:24 +0000

    Improving helper

---
+++ day1.py
@@ -0,0 +1,8 @@
+import helper
+
+def part1():
+    input = helper.get_daily_input()
+    pass
+
+def part2():
+    pass
--- helper.py
+++ helper.py
@@ -1,25 +1,30 @@
 import login_info
 import requests
+import ast
 
 current_day = 0
 
 def get_daily_input():
     try:
-        f = open("day{day}.txt".format(day=current_day), "r")
-        return f.readlines()
+        input = open("day{day}.txt".format(day=current_day), "r").readlines()
+        return [__tryeval(i) for i in input]
     except FileNotFoundError:
         uri = 'http://adventofcode.com/2022/day/{day}/input'.format(day=current_day)
         response = requests.get(uri, cookies={'session': login_info.SESSION_ID}, headers={'User-Agent': login_info.USER_AGENT})
         f = open("day{day}.txt".format(day=current_day), "w")
         f.write(response.content.decode("utf-8"))
         f.close()
-        f = open("day{day}.txt".format(day=current_day), "r")
-        return f.readlines()
+        input = open("day{day}.txt".format(day=current_day), "r").readlines()
+        return [__tryeval(i) for i in input]
 
-def get_daily_input_numbers():
-    input = get_daily_input()
-    return [int(i) for i in input]
+def __tryeval(val):
+    if val == "\n":
+        return val
 
-def get_daily_input_strings():
-    input = get_daily_input()
-    return [str(i).strip() for i in input]
+    try:
+        return ast.literal_eval(val)
+    except ValueError:
+        print(f"Error in __tryeval, failed to convert {val}")
+        pass
+
+    
